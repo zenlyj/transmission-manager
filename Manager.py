3ac8267432b45744d1aa6e7eb05eb174bf4582b2
Zen Lee <iarezenly@gmail.com>
2022-01-05 21:04:45 +0800

    Privitize functions of file manager

--- Manager.py
+++ Manager.py
@@ -4,13 +4,13 @@ from Torrent import Torrent
 
 class FileManager:
     def __init__(self, sourceDir, destinationDir, creds, dbPath):
-        self.sourceDir = sourceDir
-        self.destinationDir = destinationDir
-        self.creds = creds
-        self.hashes = self.loadHashes(dbPath)
-        self.dbPath = dbPath
+        self.__sourceDir = sourceDir
+        self.__destinationDir = destinationDir
+        self.__creds = creds
+        self.__hashes = self.__loadHashes(dbPath)
+        self.__dbPath = dbPath
 
-    def loadHashes(self, dbPath):
+    def __loadHashes(self, dbPath):
         f = None
         hashes = None
         try:
@@ -24,16 +24,16 @@ class FileManager:
         f.close()
         return hashes
 
-    def saveHashes(self):
-        f = open(self.dbPath, 'wb')
-        pickle.dump(self.hashes, f)
+    def __saveHashes(self):
+        f = open(self.__dbPath, 'wb')
+        pickle.dump(self.__hashes, f)
         f.close()
 
-    def getHashes(self, torrents):
+    def __getHashes(self, torrents):
         hashes = []
         ids = list(map(lambda torrent : torrent.getId(), torrents))
         ids = ','.join(ids)
-        infoCmd = f'transmission-remote -n \'{self.creds}\' -t {ids} -i'
+        infoCmd = f'transmission-remote -n \'{self.__creds}\' -t {ids} -i'
         infoOutput = subprocess.check_output(infoCmd, shell=True)
         info = infoOutput.decode('ASCII').split('Hash: ')
         for i in range(1, len(info)):
@@ -42,55 +42,55 @@ class FileManager:
         return hashes
 
     def download(self, magnetLink):
-        cmd = f'transmission-remote -n \'{self.creds}\' -a {magnetLink}'
+        cmd = f'transmission-remote -n \'{self.__creds}\' -a {magnetLink}'
         try:
             subprocess.check_output(cmd, shell=True)
-            downloaded = self.getClientTorrents()[-1:]
-            torrentHash = self.getHashes(downloaded)[-1]
-            self.hashes.add(torrentHash)
-            self.saveHashes()
+            downloaded = self.__getClientTorrents()[-1:]
+            torrentHash = self.__getHashes(downloaded)[-1]
+            self.__hashes.add(torrentHash)
+            self.__saveHashes()
             return True
         except subprocess.CalledProcessError as e:
             return False
 
     def transferCompletedTorrents(self):
-        torrents = self.filterTorrents(self.getClientTorrents())
+        torrents = self.__filterTorrents(self.__getClientTorrents())
         if len(torrents) == 0: return
         completedTorrents = list(filter(lambda torrent : torrent.isDone(), torrents))
         if len(completedTorrents) == 0: return
         completedPaths = list(map(lambda torrent : torrent.getPath(), completedTorrents))
         completedPaths = ' '.join(completedPaths)
-        cmd = f'mv {completedPaths} {self.destinationDir}'
+        cmd = f'mv {completedPaths} {self.__destinationDir}'
         subprocess.check_output(cmd, shell=True)
 
     def removeCompletedTorrents(self):
-        torrents = self.filterTorrents(self.getClientTorrents())
+        torrents = self.__filterTorrents(self.__getClientTorrents())
         if len(torrents) == 0: return
         completedTorrents = list(filter(lambda torrent : torrent.isDone(), torrents))
         if len(completedTorrents) == 0: return
-        completedHashes = set(self.getHashes(completedTorrents))
-        self.hashes = self.hashes.difference(completedHashes)
-        self.saveHashes()
+        completedHashes = set(self.__getHashes(completedTorrents))
+        self.__hashes = self.__hashes.difference(completedHashes)
+        self.__saveHashes()
         completedIds = list(map(lambda torrent : torrent.getId(), completedTorrents))
         completedIds = ','.join(completedIds)
-        cmd = f'transmission-remote -n \'{self.creds}\' -t {completedIds} -r'
+        cmd = f'transmission-remote -n \'{self.__creds}\' -t {completedIds} -r'
         subprocess.check_output(cmd, shell=True)
 
-    def filterTorrents(self, torrents):
+    def __filterTorrents(self, torrents):
         if len(torrents) == 0: return torrents
         # filter by hash
-        hashes = self.getHashes(torrents)
+        hashes = self.__getHashes(torrents)
         for i in range(len(hashes)):
-            if hashes[i] not in self.hashes:
+            if hashes[i] not in self.__hashes:
                 torrents.pop(i)
         return torrents
 
-    def getClientTorrents(self):
-        listCmd = f'transmission-remote -n \'{self.creds}\' -l'
+    def __getClientTorrents(self):
+        listCmd = f'transmission-remote -n \'{self.__creds}\' -l'
         listOutput = subprocess.check_output(listCmd, shell=True)
         rows = listOutput.decode('ASCII').split('\n')
         torrents = []
         for rawData in rows[1:len(rows)-2]:
-            torrent = Torrent(rawData, self.sourceDir)
+            torrent = Torrent(rawData, self.__sourceDir)
             torrents.append(torrent)
         return torrents
